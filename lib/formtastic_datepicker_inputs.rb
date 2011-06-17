aa130c6974fc5492465eada05873bbbc724421d9
gray <graycode.studio@gmail.com>
2011-06-17 15:12:04 +0300

    Using basic_input_helper

--- lib/formtastic_datepicker_inputs.rb
+++ lib/formtastic_datepicker_inputs.rb
@@ -17,14 +17,8 @@ module Formtastic
       clss << inp_opts.delete(:class).to_s
       clss << options.delete(:class).to_s
       options[:input_html] = {:class => "ui-datepicker " + clss.join(' ')}.merge inp_opts
-      options[:input_html][:value] = object.send(method).strftime("%d.%m.%Y") unless object.send(method).nil?
-      label_options = options_for_label(options)
-      label_options[:for] ||= options[:id]
-      options = options.delete(:input_html) || {}
-      options = default_string_options(method, type).merge(html_options) if [:numeric, :string, :password, :text, :phone, :search, :url, :email].include?(type)
-      field_id = generate_html_id(method, "")
-      options[:id] ||= field_id
-      label(method, label_options) << send(:text_field, method, options)
+      options[:input_html][:value] = object.send(method).try(:strftime, format)
+      basic_input_helper(:text_field, :string, method, options)
     end
 
   end
@@ -40,14 +34,8 @@ module Formtastic
       clss << inp_opts.delete(:class).to_s
       clss << options.delete(:class).to_s
       options[:input_html] = {:class => "ui-datepicker " + clss.join(' ')}.merge inp_opts
-      options[:input_html][:value] = object.send(method).strftime("%d.%m.%Y") unless object.send(method).nil?
-      label_options = options_for_label(options)
-      label_options[:for] ||= options[:id]
-      options = options.delete(:input_html) || {}
-      options = default_string_options(method, type).merge(html_options) if [:numeric, :string, :password, :text, :phone, :search, :url, :email].include?(type)
-      field_id = generate_html_id(method, "")
-      options[:id] ||= field_id
-      label(method, label_options) << send(:text_field, method, options)
+      options[:input_html][:value] = object.send(method).try(:strftime, format)
+      basic_input_helper(:text_field, :string, method, options)
     end
   end
 
