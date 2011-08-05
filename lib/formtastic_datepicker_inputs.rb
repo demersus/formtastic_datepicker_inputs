dc1816cf2245a2e875aa9c1d356c48fd659b4bbf
Axel Kelting <axel.kelting@adyard.de>
2011-08-05 12:56:18 +0200

    - converted datepicker to formtastic 2 input style

--- lib/formtastic_datepicker_inputs.rb
+++ lib/formtastic_datepicker_inputs.rb
@@ -1,48 +1,43 @@
 # FormtasticDatepickerInputs
-#To get this solution working in Rails 3, replace:
-#
-#format = options[:format] || ActiveSupport::CoreExtensions::Date::Conversions::DATE_FORMATS[:default] || ‘%d %b %Y’
-#
-#with:
-#
-#format = options[:format] || Date::DATE_FORMATS[:default] || ‘%d %b %Y’
-module Formtastic
-  if defined?(ActiveSupport::CoreExtensions)
-	DATE_FORMATS = ActiveSupport::CoreExtensions::Date::Conversions::DATE_FORMATS
-  else
-	DATE_FORMATS = Date::DATE_FORMATS
-  end
- 
-  module DatePicker
-    protected
+module Formtastic::Inputs
 
-    def date_picker_input(method, options = {})
-      format = options[:format] || DATE_FORMATS[:default] || '%d %b %Y'
-      clss = []
-      inp_opts = options.delete(:input_html) || {}
-      clss << inp_opts.delete(:class).to_s
-      clss << options.delete(:class).to_s
-      options[:input_html] = {:class => "ui-date-picker " + clss.join(' ')}.merge inp_opts
-      options[:value] = options[:value] || object.send(method).try(:strftime, format)
-      basic_input_helper(:text_field, :string, method, options)
+  class DatePickerInput < Formtastic::Inputs::StringInput
+
+    def to_html
+      input_wrapping do
+        label_html <<
+          builder.text_field(input_name, input_html_options)
+      end
+    end
+
+    def format
+      input_options[:format] || '%d %b %Y'
+    end
+
+    def value
+      object.send(method).try(:strftime, format)
+    end
+
+    def css_class
+      "ui-date-picker"
+    end
+
+    def input_html_options
+      new_class = [super[:class], css_class].compact.join(" ")
+      super.update(:class => new_class, :value => value)
     end
   end
-  
-  module DateTimePicker
-    protected
 
-    def datetime_picker_input(method, options = {}, value = nil)
-      format = options[:format] || DATE_FORMATS[:default] || '%d %b %Y %H:%M'
-      clss = []
-      inp_opts = options.delete(:input_html) || {}
-      clss << inp_opts.delete(:class).to_s
-      clss << options.delete(:class).to_s
-      options[:input_html] = {:class => "ui-date-picker " + clss.join(' ')}.merge inp_opts
-      options[:value] = options[:value] || object.send(method).try(:strftime, format)
-      basic_input_helper(:text_field, :string, method, options)
+  class DateTimePickerInput < Formtastic::Inputs::DatePickerInput
+
+    def format
+      input_options[:format] || '%d %b %Y %H:%M'
     end
+
+    def css_class
+      "ui-datetime-picker"
+    end
+
   end
+
 end
-
-Formtastic::SemanticFormBuilder.send(:include, Formtastic::DatePicker)
-Formtastic::SemanticFormBuilder.send(:include, Formtastic::DateTimePicker)
